a50e71c09ab9f874f2285522dbb4116fa5f488d6
Ogawa34 <rst34shimizuku@stu.kanazawa-u.ac.jp>
2023-07-22 21:57:30 +0900

    hitomazu

--- majan/cr1.sh
+++ majan/cr1.sh
@@ -7,37 +7,57 @@ svname="localhost"
 while true; do
 
 	#受信からの分割
-rec=$(mosquitto_sub -h "$svname" -t sc1 -C 1 )
-echo $rec
-stt=$(echo $rec | sed 's/,.*//')
+	mosquitto_sub -h "$svname" -t sc1 -C 1 > rec
+	rec=$(cat rec)
+
+	stt=$(echo $rec | sed 's/,.*//')
 echo "$stt"
 
 	if [ "$stt" == "i" ]; then
 
 		pie=$(echo $rec | sed 's/i,//g' )
-		echo $pie
+#		echo $pie
 		echo $pie > piei1
 
-		echo "your turn, select pie to throw"
-		echo "１ ２ ３ ４ ５ ６ ７ ８ ９ Ａ Ｂ Ｃ Ｄ Ｅ"
+		
+		echo "１ ２ ３ ４ ５ ６ ７ ８ ９ 10 11 12 13 14"
 		echo "｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜"
 
 		./pitenti.sh
-		#cat tentied13.txt
-		#read pinum
-exit
-		#mosquitto_pub -h localhost -t testopic -m "$(echo $pinum)"
+	sleep 1
+		mosquitto_pub -h "$svname" -t cs1 -m "ok"
+	echo ok
 	fi
 
-	if [ "$your" == "waiter" ]; then
-		:
+	if [ "$stt" == "p" ]; then
+		echo plz select
+		read pinum
+		mosquitto_pub -h "$svname" -t cs1 -m "$pinum"
+		echo sent
+	fi
+
+	if [ "$stt" == "r" ]; then
+		echo $rec | sed 's/^.,//g' | ./dict.sh
+		echo "ron?"
+		read yn
+		mosquitto_pub -h "$svname" -t cs1 -m "$yn"
+		echo sent
+	fi
+
+	if [ "$stt" == "w" ]; then
+		pie=$(echo $rec | sed 's/i,//g' )
+		echo $pie > piei1	
+		echo "１ ２ ３ ４ ５ ６ ７ ８ ９ 10 11 12 13 14"
+		echo "｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜ ｜"
+		./pitenti.sh
+	exit
 	fi
 
 	if [ "$your" == "choser" ]; then
 		echo "do you want Rong?"
 	fi
 
-	sleep 1
+
 
 done
 
--- majan/sv.sh
+++ majan/sv.sh
@@ -1,48 +1,148 @@
 #!/bin/bash
-
+svname="localhost"
+./mutter.sh
 #server
 file_path="mut"
-
+file="mut"
+#iiii
 #init1
 	echo -n "i," > sc1t
 	num_lines=$(wc -l < "$file_path")
 	echo $num_lines
-	#13こ出す
-	echo $(shuf -n 14 mut | sed -E ':a;N;$!ba;s/\n/,/g') >> sc1t
-	grep -vf sc1t "$file_path" > tmp_file
-	cat sc1t
+	line_count=$(wc -l < "$file")
+	echo -n "" > tmp
+	# ランダムな行番号を生成して、12行を取得
+	for i in {1..14}; do
+    	random_line=$((RANDOM % line_count + 1))
+    	sed -n "${random_line}p" "$file" >> tmp
+    	sed -i "${random_line}d" "$file"
+    line_count=$((line_count - 1))
+	done
+	echo $(cat tmp | sed -E ':a;N;$!ba;s/\n/,/g') >> sc1t
 	mosquitto_pub -h localhost -t sc1 -m "$(cat sc1t)"
+	rm tmp
 
 #init2
 	echo -n "i," > sc2t
 	num_lines=$(wc -l < "$file_path")
 	echo $num_lines
-	echo $(shuf -n 13 mut | sed -E ':a;N;$!ba;s/\n/,/g') >> sc2t
-	grep -vf sc2t "$file_path" > tmp_file
-	cat sc2t
+	line_count=$(wc -l < "$file")
+	echo -n "" > tmp
+	for i in {1..14}; do
+    	random_line=$((RANDOM % line_count + 1))
+    	sed -n "${random_line}p" "$file" >> tmp
+    	sed -i "${random_line}d" "$file"
+    line_count=$((line_count - 1))
+	done
+	echo $(cat tmp | sed -E ':a;N;$!ba;s/\n/,/g') >> sc2t
 	mosquitto_pub -h localhost -t sc2 -m "$(cat sc2t)"
+	rm tmp
 
 #init3
 	echo -n "i," > sc3t
 	num_lines=$(wc -l < "$file_path")
 	echo $num_lines
-	echo $(shuf -n 13 mut | sed -E ':a;N;$!ba;s/\n/,/g') >> sc3t
-	grep -vf sc3t "$file_path" > tmp_file
-	cat sc3t
+	line_count=$(wc -l < "$file")
+	echo -n "" > tmp
+	for i in {1..14}; do
+    	random_line=$((RANDOM % line_count + 1))
+    	sed -n "${random_line}p" "$file" >> tmp
+    	sed -i "${random_line}d" "$file"
+    line_count=$((line_count - 1))
+	done
+	echo $(cat tmp | sed -E ':a;N;$!ba;s/\n/,/g') >> sc3t
 	mosquitto_pub -h localhost -t sc3 -m "$(cat sc3t)"
+	rm tmp
 
 #init4
 	echo -n "i," > sc4t
 	num_lines=$(wc -l < "$file_path")
 	echo $num_lines
-	echo $(shuf -n 13 mut | sed -E ':a;N;$!ba;s/\n/,/g') >> sc4t
-	grep -vf sc4t "$file_path" > tmp_file
-	cat sc4t
+	line_count=$(wc -l < "$file")
+	echo -n "" > tmp
+	for i in {1..14}; do
+    	random_line=$((RANDOM % line_count + 1))
+    	sed -n "${random_line}p" "$file" >> tmp
+    	sed -i "${random_line}d" "$file"
+    line_count=$((line_count - 1))
+	done
+	echo $(cat tmp | sed -E ':a;N;$!ba;s/\n/,/g') >> sc4t
 	mosquitto_pub -h localhost -t sc4 -m "$(cat sc4t)"
+	rm tmp
 
+	
+	rec1=$(mosquitto_sub -h "$svname" -t cs1 -C 1 )
+#		rec2=$(mosquitto_sub -h "$svname" -t cs2 -C 1 )
+#		rec3=$(mosquitto_sub -h "$svname" -t cs2 -C 1 )
+#		rec4=$(mosquitto_sub -h "$svname" -t cs2 -C 1 )
+
+#make hab
+	cat sc1t | sed 's/^.,//g' | sed 's/,/\n/g' > hab1
+	cat sc2t | sed 's/^.,//g' | sed 's/,/\n/g' > hab2
+	cat sc3t | sed 's/^.,//g' | sed 's/,/\n/g' > hab3
+	cat sc4t | sed 's/^.,//g' | sed 's/,/\n/g' > hab4
+
+while true; do
+	if [ $rec1 == "ok" ]; then
+		sleep 1
+		break
+	fi
+		
+done	
+
+echo OK
+#pwww
+mosquitto_pub -h localhost -t sc1 -m "p"
+mosquitto_pub -h localhost -t sc2 -m "w"
+mosquitto_pub -h localhost -t sc3 -m "w"
+mosquitto_pub -h localhost -t sc4 -m "w"
+
+#削除 and ロン準備
+dlpn=$(mosquitto_sub -h "$svname" -t cs1 -C 1 )
+dlp=$(sed -n "${dlpn}p" "hab1")
+echo $dlp
+sed -i "${dlpn}d" hab1
+sleep 1
+
+#wrrr
+#mosquitto_pub -h localhost -t sc1 -m "w"
+#mosquitto_pub -h localhost -t sc2 -m "r,$dlp"
+#mosquitto_pub -h localhost -t sc3 -m "r,$dlp"
+#mosquitto_pub -h localhost -t sc4 -m "r,$dlp"
+mosquitto_pub -h localhost -t sc1 -m "r,$dlp"
+
+
+echo -n $(mosquitto_sub -h "$svname" -t cs1 -C 1) > cs
+#echo -n $(mosquitto_sub -h "$svname" -t cs2 -C 1) >> cs
+#echo -n &(mosquitto_sub -h "$svname" -t cs3 -C 1) >> cs
+#echo -n $(mosquitto_sub -h "$svname" -t cs4 -C 1) >> cs
+if [ "$cs" == "nnnn" ]; then
+	echo $dlp >> mut
+elif [ "$cs" == "y" ]; then
+	echo $dip >> hab1
+fi
+
+mosquitto_pub -h localhost -t sc1 -m "w"
+mosquitto_pub -h localhost -t sc2 -m "w"
+mosquitto_pub -h localhost -t sc3 -m "w"
+mosquitto_pub -h localhost -t sc4 -m "w"
+
+
+#p1
+	echo -n "i,0" > sc1t
+	echo -n "w,0" > sc2t
+	echo -n "w,0" > sc3t
+	echo -n "w,0" > sc4t
 
 exit
 while true; do
+
+
+
+
+
+
+
 
 
 sleep 3
